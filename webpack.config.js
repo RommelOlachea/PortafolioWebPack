c1017ede9307d0c0854ece5497b06363329bc875
RommelOlachea <msc.rommel.olachea@gmail.com>
2022-04-01 15:00:15 -0700

    loader imagenes

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); //agregamos el plugin de css al documento
+const CopyPlugin = require('copy-webpack-plugin'); //agregamos el soporte para copy webpack, para copiar archivos en la carp. dist
 
 module.exports = {
     entry: './src/index.js',
@@ -19,6 +21,11 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
+            },
+            {   //creamos la configuracion para el loader de css
+                test: /\.css|.styl$/i, //se agrego la extension styl que corresponde a stylys
+                use: [MiniCssExtractPlugin.loader,
+                'css-loader','stylus-loader'], //asi mismo se agrego la configuracion para el loader de stylus
             }
         ]
     },
@@ -27,6 +34,15 @@ module.exports = {
             inject: true,
             template: './public/index.html',
             filename: './index.html'
+        }),
+        new MiniCssExtractPlugin(), //agregamos el recurso del plugin 
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname,"src", "assets/images"),
+                    to: "assets/images"
+                }
+            ]
         })
     ]
 }
